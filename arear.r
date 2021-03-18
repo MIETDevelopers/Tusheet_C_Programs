e4c362c677367ff6b5587a5ebf9bc482c55545a9
12345678-man <76225239+12345678-man@users.noreply.github.com>
2021-03-18 16:30:34 +0530

    rectangle

---
+++ arear.r
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+int main()
+{
+    float length,breadth;
+    float area;
+    clrscr();
+ 
+    printf(" Enter the Length of a Rectangle : ");
+    scanf("%f",&length);
+ 
+    printf("\n Enter the Breadth of a Rectangle : ");
+    scanf("%f",&breadth);
+ 
+    area = length * breadth;
+ 
+    printf("\n Area of Rectangle is : %f",area);
+    return 0;
+}
---
+++ rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float l,b,area;
+ 
+    printf("Enter the value of length: ");
+    scanf("%f",&l);
+ 
+    printf("Enter the value of breadth: ");
+    scanf("%f",&b);
+ 
+    area=l*b;
+ 
+    printf("Area of rectangle: %f\n",area);
+     
+    return 0;
+}
