54c84b0ce2d2e11a8cecaf627d23713e60cdd162
12345678-man <76225239+12345678-man@users.noreply.github.com>
2021-03-30 22:10:19 +0530

    File Updated

--- stringpointers.c
+++ stringpointers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
-  char name[] = "Ishav Verma";
+  char name[] = "Tusheet";
 
   printf("%c \n", *name);     	// Output will be I
   printf("%c \n", *(name+3));	// Output will be A
@@ -15,4 +15,3 @@ int main(void) {
   printf("%c \n", *(namePtr+2)); // Output will be H
   printf("%c \n", *(namePtr+7)); // Output will be E
 } 
- 
