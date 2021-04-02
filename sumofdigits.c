39fdf641f5d7e00d634f2f29b3b8a30cb5f96357
12345678-man <76225239+12345678-man@users.noreply.github.com>
2021-04-02 17:47:10 +0530

    File Updated

--- sumofdigits.c
+++ sumofdigits.c
@@ -5,10 +5,11 @@ int main()
   int Number, Reminder, Sum=0;
 
   printf("\n Please Enter any number\n");
-  scanf("%d", &Number);
+  scanf("%d", &Number);//This will scan(read)the number enetered by the user.
 
-  while(Number > 0)
+  while(Number > 0)//Using while loop.
   {
+    //This is the formula to calculate the sum of digits.
      Reminder = Number % 10;
      Sum = Sum+ Reminder;
      Number = Number / 10;
